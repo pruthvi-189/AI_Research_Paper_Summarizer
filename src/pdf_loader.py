57324f96eefe8f89db95661aeaea3d645b4512da
Pruthvi Narayana Reddy <pruthvinarayanareddy@gmail.com>
2025-12-21 21:36:05 +0530

    Update pdf_loader.py

--- src/pdf_loader.py
+++ src/pdf_loader.py
@@ -1,1 +1,10 @@
+import pdfplumber
 
+def extract_text_from_pdf(pdf_file):
+    full_text = ""
+    with pdfplumber.open(pdf_file) as pdf:
+        for page in pdf.pages:
+            text = page.extract_text()
+            if text:
+                full_text += text + "\n"
+    return full_text
